3226c4726c499cbc4faa961ffa9e95853ffb2a9e
rvlacil <radek.vlacil@gmail.com>
2020-04-27 01:54:03 +0200

    Add HttpMock aggregate class

--- SharpMock.Http.Test/HttpRequestMessageTest.cs
+++ SharpMock.Http.Test/HttpRequestMessageTest.cs
@@ -9,7 +9,7 @@ namespace SharpMock.Http.Test
     public class HttpRequestMessageTest
     {
         [Test]
-        public void WhenBuildToStringIsCorrect()
+        public void WhenBuildWithHeadersAndBodyToStringIsCorrect()
         {
             var rqst = new HttpRequestMessageBuilder().Method("GET").Path("/test").AddHeader("Test", "test1").Body("testBody").Build();
 
@@ -18,6 +18,26 @@ namespace SharpMock.Http.Test
 Test: test1
 
 testBody";
+            Assert.AreEqual(expected, rqst.ToString());
+        }
+
+        [Test]
+        public void WhenBuildWithHeadersToStringIsCorrect()
+        {
+            var rqst = new HttpRequestMessageBuilder().Method("GET").Path("/test").AddHeader("Test", "test1").Build();
+
+            var expected =
+@"GET /test
+Test: test1";
+            Assert.AreEqual(expected, rqst.ToString());
+        }
+
+        [Test]
+        public void WhenBuildToStringIsCorrect()
+        {
+            var rqst = new HttpRequestMessageBuilder().Method("GET").Path("/test").Build();
+
+            var expected = "GET /test";
             Assert.AreEqual(expected, rqst.ToString());
         }
     }
