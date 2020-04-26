ea17c03b358b8c2b23552e54f12db658e3eed038
rvlacil <radek.vlacil@gmail.com>
2020-04-27 00:29:38 +0200

    Annotate with exclude from coverage

--- SharpMock.Http/HttpRequestMessageBuilder.cs
+++ SharpMock.Http/HttpRequestMessageBuilder.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace SharpMock.Http
 {
+    [ExcludeFromCodeCoverage]
     public class HttpRequestMessageBuilder
     {
         private string _method = "GET";
